531d7b795b3466a0027c3551c7cdef98289cbd23
Voidilov <voidilov@gmail.com>
2020-08-08 14:07:41 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
             targets: ["VDAsync"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/dankinsoid/UnwrapOperator.git", from: "0.5.0"),
         .package(url: "https://github.com/google/promises.git", from: "1.2.8")
     ],
     targets: [
@@ -20,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VDAsync",
-            dependencies: ["UnwrapOperator", "Promises"]),
+            dependencies: ["Promises"]),
         .testTarget(
             name: "VDAsyncTests",
             dependencies: ["VDAsync"]),
