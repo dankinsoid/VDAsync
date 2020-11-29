d7f68eb2bb8d96e13d63f32857a6656b7f49caec
Danil <voidilov@gmail.com>
2020-11-29 15:26:13 +0300

    0.23.1

--- Sources/VDAsync/Async.swift
+++ Sources/VDAsync/Async.swift
@@ -9,30 +9,32 @@ import Foundation
 
 public enum Async {
     
-    @discardableResult
-    public static func execute(on queue: DispatchQueue = DispatchQueue.global(qos: .utility), _ block: @escaping () throws -> ()) -> Async.Catch<Error> {
-        return queue.async(block)
-    }
+	@discardableResult
+	public static func execute(on queue: DispatchQueue = DispatchQueue.global(qos: .utility), _ block: @escaping () throws -> ()) -> Async.Catch<Error> {
+		return queue.async(block)
+	}
     
-    public static func execute(on queue: DispatchQueue = DispatchQueue.global(qos: .utility), _ block: @escaping () -> ()) {
-        queue.async(execute: block)
-    }
+	public static func execute(on queue: DispatchQueue = DispatchQueue.global(qos: .utility), _ block: @escaping () -> ()) {
+		queue.async(execute: block)
+	}
     
 }
 
 extension Async {
 
-    public final class Catch<Failure: Error> {
-        internal var queue: DispatchQueue?
-        internal var block: ((Failure) -> ())?
+	public final class Catch<Failure: Error> {
+		internal var queue: DispatchQueue?
+		internal var block: ((Failure) -> ())?
         
-        public func `catch`(_ block: @escaping (Failure) -> ()) {
-            if let queue = self.queue {
-                self.block = { e in queue.async { block(e) } }
-            } else {
-                self.block = block
-            }
-        }
-    }
+		init() {}
+		
+		public func `catch`(_ block: @escaping (Failure) -> ()) {
+			if let queue = self.queue {
+				self.block = { e in queue.async { block(e) } }
+			} else {
+				self.block = block
+			}
+		}
+	}
     
 }
--- Sources/VDAsync/GDC++.swift
+++ Sources/VDAsync/GDC++.swift
@@ -9,46 +9,62 @@ import Foundation
 
 extension DispatchQueue {
     
-    @discardableResult
-    public func async(_ block: @escaping () throws -> ()) -> Async.Catch<Error> {
-        let catchError = Async.Catch<Error>()
-        self.async(execute: {
-            do {
-                try block()
-            } catch {
-                catchError.block?(error)
-            }
-        })
-        return catchError
-    }
+	@discardableResult
+	public func async(_ block: @escaping () throws -> Void) -> Async.Catch<Error> {
+		let catchError = Async.Catch<Error>()
+		self.async(execute: {
+			do {
+				try block()
+			} catch {
+				catchError.block?(error)
+			}
+		})
+		return catchError
+	}
     
 }
 
-
 extension DispatchQueue {
+	
+	public func sync<T>(_ block: () -> T) -> T {
+		var result: T?
+		self.sync {
+			result = block()
+		}
+		return result!
+ 	}
+ 
+	public func asyncBlock<T>(_ block: @escaping (T) -> Void) -> (T) -> Void {
+		return { data in
+			self.async {
+				block(data)
+			}
+		}
+	}
     
-    public func sync<T>(_ block: () -> T) -> T {
-        var result: T?
-        self.sync {
-            result = block()
-        }
-        return result!
-    }
-    
-    public func asyncBlock<T>(_ block: @escaping (T) -> ()) -> (T) -> () {
-        return { data in
-            self.async {
-                block(data)
-            }
-        }
-    }
-    
-    public func asyncBlock(_ block: @escaping () -> ()) -> () -> () {
-        return {
-            self.async {
-                block()
-            }
-        }
-    }
-    
+	public func asyncBlock(_ block: @escaping () -> Void) -> () -> Void {
+		return {
+			self.async {
+				block()
+			}
+		}
+	}
+	
+	public func barrier(_ execute: @escaping () -> Void) {
+		async(group: nil, qos: qos, flags: .barrier, execute: execute)
+	}
+	
+	@discardableResult
+	public func barrier(_ execute: @escaping () throws -> Void) -> Async.Catch<Error> {
+		let catchError = Async.Catch<Error>()
+		async(group: nil, qos: qos, flags: .barrier) {
+			do {
+				try execute()
+			} catch {
+				catchError.block?(error)
+			}
+		}
+		return catchError
+	}
+	
 }
